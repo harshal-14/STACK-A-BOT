15dc10892a2ef156aac3928118406a0a926edeb2
riblair <rileypatblair@gmail.com>
2025-04-12 17:17:06 -0400

    Created Position monitoring thread, wrote send-receive serial procedure, and wrote test infra for HW Manipulator.

--- Components/Hardware/HwManipulator.py
+++ Components/Hardware/HwManipulator.py
@@ -6,6 +6,8 @@ from ...Utilities import joint_array_sanitizer
 import glob
 import serial
 import sys
+import threading
+import time
 
 class HwManipulator(Manipulator):
     """Low-level integration & communication with Manipulator.
@@ -21,26 +23,42 @@ class HwManipulator(Manipulator):
 
     """ LIST OF GCODE CMDS to test:
         G0 AX BX...: Move Uninterpolated
+        ?: Getting Information
+        !: feed hold
 
-        M114: Get current Position
+        M114: Get current Position - alternative to ?
+        M0: Stop  - alternative to !
+
         M117: Get Zero Position
         M119: Get Endstop Status
-        ?: Getting Information. Unclear of use, but it is called in asgard
         G28: Go to Origin (Home)
-        M0: Stop 
         M18: Disable all stepper motors
         $H: homing?
         $X: Kill Alarm Command?
+        G90: Set to Absolute Positioning 
+        G91: Set to Relative Positioning
     """
-    # Should add in some checking to ensure if/when read values differ from expected (not received or garbage), we should disconnect. 
     def __init__(self, ):
         print("HwManipulator")
         self.connection = serial.Serial(baudrate=115200)
         self.connected = False
+        self.current_position = None
+        # Threading objects
+        self.position_thread = threading.Thread(None, self.update_position, "Position thread")
+        self._pos_lock = threading.Lock()
+        self._rw_lock = threading.Lock()
 
+    def __del__(self):
+        self.stop()
+
+    def stop(self):
+        if self.connected:
+            self.connection.write("!".encode("UTF-8"))
+    
     def connect(self, **kwargs) -> int:
         self.connection.port = kwargs["port"]
         self.connection.open()
+        self.position_thread.start()
         self.connected = True
         return 0
 
@@ -63,33 +81,92 @@ class HwManipulator(Manipulator):
         if self.connected:
             self.connection.close()
             self.connected = False
+            try:
+                self.position_thread.join(timeout=2)
+            except RuntimeError as e:
+                print(e)
+                return -1
         return 0
 
     def move_js(self, q_array: np.ndarray):
+        """GRBL reccomends different strategies for sending GCode to the machine [seen here](https://github.com/grbl/grbl/wiki/Interfacing-with-Grbl).\n 
+            First method - Simple Send-Response. 
+            \nSend command and wait for ok response before sending another one
+                \n\tPros: 
+                     * Ensures sent package wasn't garbled by communication interface. 
+                     * Ensures that sent command is parsable and no errors are thrown for a faulty command.
+                     * Running other methods won't accidentially receive wrong response msg?
+                \n\tCons: 
+                     * Slow and potentially jerky motion when running with small differences in joint values
+
+            Second method - Streming via Flow Control
+            \nAllows commands to be sent uninterrupted until a special XON/XOFF signal has been sent. Dependant on the state of the receiving buffer
+                \n\tPros: 
+                     * Takes full advantage of the look-ahead buffer for smooth motion.
+                     * Potentially better for our non-blocking architecture assuming we aren't bottlenecked by loop frequency.
+                \n\tCons: 
+                     * Broken depending on specific Arduino USB-serial converter chip. (Need to verify)
+                     * Not as robust as method 1. May need system specific tuning of high-low watermarks. 
+                     * Not officially reccomended by GRBL team. 
+                     * May need its own thread / logic to prevent other methods from accidentially receiving XON/XOFF characters. 
+        
+            Third Method - Character-Counting.
+            \nHost keeps a running total of characters sent via serial and only decides to send another message once there is enough room in the buffer.
+            \nWhen grbl responds, we know that a command has been processed and the data has been removed from the rx-buffer.
+                \n\tPros: 
+                     * Safe like Method 1 and optimal like method 2.
+                     * "Not hard" to implement (see their example code in stream.py)   
+                \n\tCons: 
+                     * Unsure how it will interface w/ other methods. Will we need special RX/TX handler functions?
+        """     
+
         qs = joint_array_sanitizer(q_array)
         # Rapid uninterpolated movement        B and C must be equivalent...
         message = ( f"G0 A{str(qs[0])} B{str(qs[1])} C{str(qs[1])} D{str(qs[2])}"
                       f" X{str(qs[3])} Y{str(qs[4])} Z{str(qs[5])}\n")
         
-        self.connection.write(message.encode('UTF-8'))
-        print(self.connection.readline())
-        # if message is "OK" then we know it worked
+        with self._rw_lock:
+            self.connection.write(message.encode("UTF-8"))
+            retmsg = self.connection.readline()
+        if retmsg != "ok":
+            raise RuntimeError("HWManipulator.move_js() returned something other than 'ok'.\n"
+                               f"potential fault msg: {retmsg}")
 
     def move_ts(self, pose:Pose):
         ikpy_joints = self.IK_Solver(pose)
         self.move_js(ikpy_joints)
+
+    def update_joint_values(self):
+        """ Function to query grbl for current position, 
+            \nAs reccomended by GRBL documentation, this runs at 10Hz max, any faster yields diminshing returns.
+        """
+        while True:
+            if not self.connected: break
+            with self._rw_lock:
+                self.connection.write("?\n".encode('UTF-8'))
+                dataRead = str(self.connection.readline())
+            if "error" in dataRead or "ALARM" in dataRead:
+                print(f"Error encountered: {dataRead}")
+            else:
+                raw_data = dataRead[1:][:-1].split(",")
+                qs = np.array([[float(raw_data[1][5:][:-2])], [float(raw_data[2][:-2])], [float(raw_data[4][:-2])]
+                           [float(raw_data[5][:-2])],     [float(raw_data[6][:-2])], [float(raw_data[7][:-2])]])
+                qs *= (np.pi/180.0)
+                with self._pos_lock:
+                    self.current_position = qs
+            time.sleep(0.1) # sleep for 100ms 
     
     def get_joint_values(self) -> np.ndarray:
         """ Use Gcode M114 to get a string w/ current positions. 
             May also try the "?" Command as this is what Asgard used
+
+            So Grbl reccomends only querying the position at 5Hz-10Hz maximum.
+            Perhaps a thread is neccesary here to continually get the position and update its associated variable. 
         """
-        self.connection.write("?\n".encode('UTF-8'))
-        dataRead = str(self.connection.readline())
-        raw_data = dataRead[1:][:-1].split(",")
-        qs = np.array([[float(raw_data[1][5:][:-2])], [float(raw_data[2][:-2])], [float(raw_data[4][:-2])]
-                       [float(raw_data[5][:-2])],     [float(raw_data[6][:-2])], [float(raw_data[7][:-2])]])
-        qs *= (np.pi/180.0)
-        return qs
+        retval = None
+        with self._pos_lock:
+            retval = self.current_position
+        return retval
     
     def get_serial_port(self) -> str:
         """ Lists serial port names. Method taken from Asgard1.0 serial_port_finder.py 
--- Tests/test_hw_communication.py
+++ Tests/test_hw_communication.py
@@ -1,63 +1,177 @@
+""" File containing test functions for HW Manipulator"""
+from ..Components.Hardware.HwManipulator import HwManipulator
+from ..Runtime_Handling.Routines.Manipulation.LinearInterpolationJS import LinearInterpolationJS
+from ..Runtime_Handling.Routines.Manipulation.LinearInterpolationTS import LinearInterpolationTS
+from ..Runtime_Handling.RoutineScheduler import RoutineScheduler
+from ..World.Geometry import Pose
 
-### Testing Plan for the HW Manipulator
+from scipy.spatial.transform import Rotation as R
+import time
+import numpy as np
+import cv2
 
-""" [Test 1] Unpowered reading of Joint Values
-    Connect to, and read out Joint Values from HW Manipulator
-        Connection should not fail
-        Should Receive all 0's. 
-"""
+# Globals used for each test
+manip = HwManipulator()
+scheduler = RoutineScheduler()
 
-""" [Test 2] Unpowered moveJS()
-    Connect to, and write joints to arbitrary valid joint values. 
-        Connection should not fail
-        Should receive success response "ok" from Arduino
-"""
+# Defining helper functions
 
-"""[Test 3] Powered move_js()
-    Turn off Power
-    Move Robot to "Home Position", and Turn power on
-    Connect to, and use LinearInterpolationJS to move robot to a set non-zero position
-        Manipulator Should physically move from Home to non-zero position
-"""
+def powered_startup():
+    print("Power off robot, move it to the zero position, and then turn the robot back on. Press any key to continue.")
+    cv2.waitKey(0)
+    print("Take a step back from the robot!")
+    time.sleep(2)
 
-"""[Test 4] Powered move_js() and drift analysis
-    Turn off Power
-    Move Robot to "Home Position", and Turn power on
-    Connect to, and use LinearInterpolationJS to move robot to a set non-zero position
-    Call LinearInterpolationJS to move robot back to zero position. 
-        Manipulator should physically move from home to non-zero position
-        Manipulator should return exactly to the home position unimpeded by friction and with no gear slippage
-"""
 
-"""[Test 5] Powered move_ts()
-    Turn off Power
-    Move Robot to "Home Position", and Turn power on
-    Connect to, and use LinearInterpolationJS to move robot away from home
-    use LinearInterpolationTS to move robot to non-singulatity position
-        Manipulator should exhibit smooth motion in both joint space, and task space control modes.  
-        Manipulator should NOT attempt to move to a different configuration ("Elbow down" vs "Elbow up") during Task-space interpolation
-"""
+def test_zero():
+    """ [Test 0] Unpowered reading of Joint Values\n
+        Connect to, and read out Joint Values from HW Manipulator
+            * Connection should not fail
+            * Should Receive all 0's. 
+    """
+    assert(manip.bringup() == 0)
+    time.sleep(1) # give time for the first read of a position to occur
+    cur_pos = manip.get_joint_values()
+    assert(cur_pos != None)
+    assert(cur_pos.shape == (6,1))
+    assert(np.linalg.norm(cur_pos) < 0.01)
 
-# TODO: Need to write special routine to test endstops...
-"""[Test 6] endstop tests?
-    Turn off Power
-    Move Robot to "Home Position", and Turn power on
-    Connect to, and use {endstop_test_routine}
-    Carefully trigger each endstop
-        Endstops should react to being pressed,
-        Motion from joints should stop when their respective endstop is triggered. 
-"""
+    assert(manip.disconnect() == 0)
 
-# TODO: Need to write Routine that accepts a string as a custom msg to send to the Manipulator. See HWManipulator for list of testable GCode commands
-"""[Test 7] Custom GCode msg testing routine
-    Connect to Robot
-    Run {custom_gcode_routine} and observe output
-        Output matched desired behavior...
-"""
+def test_one():
+    """ [Test 1] Unpowered moveJS()\n
+        Connect to, and write joints to arbitrary valid joint values.
+            * Connection should not fail
+            * Should receive success response "ok" from Arduino
+    """
+    assert(manip.bringup() == 0)
+    time.sleep(1) # give time for the first read of a position to occur
+    try:
+        manip.move_js(np.zeros((6,1)))
+    except RuntimeError as e:
+        print(e)
+        assert(False)
 
-# IF we decide to write the safety Daemon, this is how we would test it. This could be its own test file if we expand its behavior 
-"""[Test 7] Unpowered Safety Daemon Checker 
-    Connect to, and send faulty (defined by tester) move command. 
-        Safety Daemon should prevent faulty move from being executed. 
-        Satefy Daemon should gracefully stop Manipulator.
-"""
+    assert(manip.disconnect() == 0)
+
+def test_two():
+    """[Test 2] Powered move_js()\n
+        Turn off Power\n
+        Move Robot to "Home Position", and Turn power on\n
+        Connect to, and use LinearInterpolationJS to move robot to a set non-zero position
+            * Manipulator Should physically move from Home to non-zero position
+    """
+    dst_q = np.array([[np.pi/12], [np.pi/12], [np.pi/12], [np.pi/12], [np.pi/12], [np.pi/12]])
+    move_routine = LinearInterpolationJS(dst_q, 3)
+
+    powered_startup()
+    RoutineScheduler.run_routines([move_routine])
+
+    cur_pos = manip.get_joint_values()
+    assert(np.linalg.norm(cur_pos-dst_q) < 0.1)
+    assert(manip.disconnect() == 0)
+
+def test_three():
+    """[Test 3] Powered move_js() and drift analysis\n
+        Turn off Power\n
+        Move Robot to "Home Position", and Turn power on\n
+        Connect to, and use LinearInterpolationJS to move robot to a set non-zero position\n
+        Call LinearInterpolationJS to move robot back to zero position. 
+            * Manipulator should physically move from home to non-zero position
+            * Manipulator should return exactly to the home position unimpeded by friction and with no gear slippage
+    """
+    dst_q = np.array([[np.pi/12], [np.pi/12], [np.pi/12], [np.pi/12], [np.pi/12], [np.pi/12]])
+    zero_q = np.zeros((6,1))
+    move_routine = LinearInterpolationJS(dst_q, 3)
+    move_back_routine = LinearInterpolationJS(zero_q, 3)
+
+    powered_startup()
+    RoutineScheduler.run_routines([move_routine, move_back_routine])
+    
+    cur_pos = manip.get_joint_values()
+
+    assert(np.linalg.norm(cur_pos) < 0.01)
+    print("Is the end effector back into the home position [Y/n]")
+    key = cv2.waitKey(0) & 0xFF
+    assert(key == ord('Y') or key == ord('y'))
+
+def test_four():
+    """[Test 4] Powered move_ts()\n
+        Turn off Power\n
+        Move Robot to "Home Position", and Turn power on\n
+        Connect to, and use LinearInterpolationJS to move robot away from home\n
+        use LinearInterpolationTS to move robot to non-singulatity position
+            * Manipulator should exhibit smooth motion in both joint space, and task space control modes.  
+            * Manipulator should NOT attempt to move to a different configuration ("Elbow down" vs "Elbow up") during Task-space interpolation
+    """
+    js_dst_q = np.array([[np.pi/12], [np.pi/12], [np.pi/12], [np.pi/12], [np.pi/12], [np.pi/12]])
+    ee_p1  = Pose(R.from_euler('xyz', [0, np.pi, 0]).as_matrix(), [0.3,  0.0, 0.2])
+    move_js = LinearInterpolationJS(js_dst_q, 3)
+    move_ts = LinearInterpolationTS(ee_p1, 3)
+    
+    powered_startup()
+    RoutineScheduler.run_routines([move_js, move_ts])
+    p1_q = manip.IK_Solver(ee_p1)
+    cur_pos = manip.get_joint_values()
+    assert(np.linalg.norm(p1_q-cur_pos) < 0.1)
+    
+    assert(manip.disconnect() == 0)
+
+def test_five():
+    # TODO: Need to write special routine to test endstops...
+    """[Test 5] endstop tests?\n
+        Turn off Power\n
+        Move Robot to "Home Position", and Turn power on\n
+        Connect to, and use {endstop_test_routine}\n
+        Carefully trigger each endstop
+            * Endstops should react to being pressed,
+            * Motion from joints should stop when their respective endstop is triggered. 
+    """
+    pass
+
+def test_six():
+    # TODO: Need to write Routine that accepts a string as a custom msg to send to the Manipulator. See HWManipulator for list of testable GCode commands
+    """[Test 6] Custom GCode msg testing routine\n
+        Connect to Robot\n
+        Run {custom_gcode_routine} and observe output
+            * Output matched desired behavior...
+    """
+    pass
+
+def test_seven():
+    # IF we decide to write the safety Daemon, this is how we would test it. This could be its own test file if we expand its behavior 
+    """[Test 7] Unpowered Safety Daemon Checker\n
+        Connect to, and send faulty (defined by tester) move command. 
+            * Safety Daemon should prevent faulty move from being executed. 
+            * Satefy Daemon should gracefully stop Manipulator.
+    """
+    pass
+
+def main(args):
+    print(f"Running test {args.test_num}")
+    if args.test_num == 0:
+        test_zero()
+    elif args.test_num == 1:
+        test_one()
+    elif args.test_num == 2:
+        test_two()
+    elif args.test_num == 3:
+        test_three()
+    elif args.test_num == 4:
+        test_four()
+    elif args.test_num == 5:
+        test_five()
+    elif args.test_num == 6:
+        test_six()
+    elif args.test_num == 7:
+        test_seven()
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test_num", type=int, default=0, help="Dictates which test to run. Valid are [0-7]")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
