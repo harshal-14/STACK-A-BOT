1f4a570d3834523f2528912c288666c820913a40
riblair <rileypatblair@gmail.com>
2025-04-25 21:27:35 -0400

    Ready for end_effector and hw_interface testing

--- Components/Hardware/HwEndEffector.py
+++ Components/Hardware/HwEndEffector.py
@@ -1,4 +1,6 @@
 from ..EndEffector import EndEffector
+from .HwInterface import HwInterface
+from ..SingletonRegistry import get_singleton
 
 class HwEndEffector(EndEffector):
     """Low-level integration & communication with vacuum pump and switch. 
@@ -7,24 +9,39 @@ class HwEndEffector(EndEffector):
     
     Attributes:
         status (int): refers to status of pump. Either pulling vacuum (1) or passive (0)
+        hw_interface(HWInterface): the serial connection for which we send status commands to
     """
-    # TODO: Implement
     def __init__(self):
-        print("HwEndEffector")
         self.status = False
-        pass
+        self.hw_interface:HwInterface
 
     def bringup(self, **kwargs) -> int:
-        return self.connect(kwargs=kwargs)
-
+        self.hw_interface = get_singleton(HwInterface)
+        if self.connect() == -1:
+            return -1
+        # set pump OFF and wait for "ok" response
+        retstr = self.hw_interface.tx_rx("M3 S0")
+        if "error" in retstr or "ALARM" in retstr:
+            raise RuntimeError("HWManipulator.update_joint_values() returned something other than 'ok'.\n"
+                              f"potential fault msg: {retstr}")
+        return 0
+    
     def connect(self, **kwargs) -> int:
-        return 0
-    
-    def disconnect(self, **kwargs) -> int:
-        return 0
+       return self.hw_interface.connect_device()
 
-    def get_status(self) -> int:
+    def disconnect(self, **kwargs) -> int:
+        if self.hw_interface.connected:
+            self.set_mode(False)
         return 0
+    def get_status(self) -> int:
+        return self.status
     
-    def set_mode(self, on:int):
-        pass
+    def set_mode(self, new_status:bool):
+        if new_status:
+            retstr = self.hw_interface.tx_rx("M3 S1000")
+        else:
+            retstr = self.hw_interface.tx_rx("M3 0")
+        if retstr == "ok":
+            self.status = new_status
+        else:
+            print(f"Error encountered: {retstr}")
--- Components/Hardware/HwManipulator.py
+++ Components/Hardware/HwManipulator.py
@@ -1,13 +1,14 @@
 from ..Manipulator import Manipulator 
+from .HwInterface import HwInterface
+from ..SingletonRegistry import get_singleton
 import numpy as np
-from ...World.Geometry import Pose, Point 
+from ...World.Geometry import Pose
 from ...Utilities import joint_array_sanitizer
 
-import glob
-import serial
-import sys
 import threading
 import time
+
+MAX_ATTEMPTS = 5
 
 class HwManipulator(Manipulator):
     """Low-level integration & communication with Manipulator.
@@ -17,129 +18,70 @@ class HwManipulator(Manipulator):
     HwManipulator is a singleton object and can be refered to via `SingletonRegistry.get_singleton(Manipulator)`. 
     
     Attributes:
-        connection (serial.Serial): Serial connection to Manipulator Arduino
-        connected (bool): Status of Connection. Coupled only via calles to connect() and disconnect()
+        hw_interface(HWInterface): The serial connection for which we send status commands.
+        current_position (np.ndarray): (6,1) Position of joints.
     """
-
-    """ All of the following work:
-        G0 AX BX...: Move Uninterpolated
-        ?: Getting Information
-        !: feed hold - might want to use M1 instead
-        ~: resumes control, 
-        G90: Set to Absolute Positioning 
-        G91: Set to Relative Positioning
-        M1: Stop  - alternative to ! (probably works)
-        $X: Kill Alarm Command?
-        G28: Go to Origin (Home)
-        """
     def __init__(self, ):
-        print("HwManipulator")
-        self.connection = serial.Serial(baudrate=115200)
-        self.connected = False
+        self.hw_interface:HwInterface
         self.current_position = None
         # Threading objects
         self.position_thread = threading.Thread(None, self.update_joint_values, "Position thread")
         self._pos_lock = threading.Lock()
-        self._rw_lock = threading.Lock()
+        self.stop_thread = False
 
     def __del__(self):
         self.stop()
 
     def stop(self):
-        if self.connected:
-            self.connection.write("!".encode("UTF-8"))
+        if self.hw_interface.connected:
+            self.hw_interface.tx_rx("!")
     
-    def connect(self, **kwargs) -> int:
-        self.connection.port = kwargs["port"]
-        self.connection.open()
-        # make two calls to read, one to get first empty msg, and the second to get the intro sequence
-        dataRead = str(self.connection.readline().decode("utf-8")).strip("\r\n")
-        dataRead = str(self.connection.readline().decode("utf-8")).strip("\r\n")
-        print(dataRead)
-        self.connected = True
-        self.position_thread.start()
-        return 0
-
     def bringup(self, **kwargs) -> int:
-        port = self.get_serial_port()
-        if port == []:
-            print("Unable to locate open serial port.")
+        self.hw_interface = get_singleton(HwInterface)
+        if not self.connect():
             return -1
-        elif len(port) > 1:
-            print(f"WARNING: More than one potential serial port matches for HW Manipulator.\n",
-                    f"\t Trying with '{port[0]}'")
-        try:
-            self.connect(port=port[0])
-        except Exception as e:
-            print(f"Exception Thrown: {e}")
-            return -1
-        
         # wait until we get a successful readout of our current position
         position_iter_attempts = 0
-        while True:
+        while position_iter_attempts < MAX_ATTEMPTS:
             with self._pos_lock:
                 if(self.current_position is not None): break
             position_iter_attempts+=1
-            if(position_iter_attempts > 5):
-                print("Unable to read position from Manipulator")
-                return -1
             time.sleep(0.5)
+
+        if position_iter_attempts == MAX_ATTEMPTS:
+            print("Unable to read position from Manipulator")
+            return -1
+        return 0
+    
+    def connect(self, **kwargs) -> int:
+        if not self.hw_interface.connect_device():
+            return -1
+        self.position_thread.start()
         return 0
 
     def disconnect(self, **kwargs) -> int:
-        if self.connected:
-            self.connected = False
-            try:
-                self.position_thread.join(timeout=2)
-            except RuntimeError as e:
-                print(e)
-                return -1
-            self.connection.close()
-        return 0
+        self.stop_thread = True
+        retval = 0
+        try:
+            self.position_thread.join(timeout=2)
+        except RuntimeError as e:
+            print(e)
+            retval = -1
+        self.stop_thread = False
+        return retval
 
     def move_js(self, q_array: np.ndarray):
-        """GRBL reccomends different strategies for sending GCode to the machine [seen here](https://github.com/grbl/grbl/wiki/Interfacing-with-Grbl).\n 
-            First method - Simple Send-Response. 
-            \nSend command and wait for ok response before sending another one
-                \n\tPros: 
-                     * Ensures sent package wasn't garbled by communication interface. 
-                     * Ensures that sent command is parsable and no errors are thrown for a faulty command.
-                     * Running other methods won't accidentially receive wrong response msg?
-                \n\tCons: 
-                     * Slow and potentially jerky motion when running with small differences in joint values
-
-            Second method - Streming via Flow Control
-            \nAllows commands to be sent uninterrupted until a special XON/XOFF signal has been sent. Dependant on the state of the receiving buffer
-                \n\tPros: 
-                     * Takes full advantage of the look-ahead buffer for smooth motion.
-                     * Potentially better for our non-blocking architecture assuming we aren't bottlenecked by loop frequency.
-                \n\tCons: 
-                     * Broken depending on specific Arduino USB-serial converter chip. (Need to verify)
-                     * Not as robust as method 1. May need system specific tuning of high-low watermarks. 
-                     * Not officially reccomended by GRBL team. 
-                     * May need its own thread / logic to prevent other methods from accidentially receiving XON/XOFF characters. 
-        
-            Third Method - Character-Counting.
-            \nHost keeps a running total of characters sent via serial and only decides to send another message once there is enough room in the buffer.
-            \nWhen grbl responds, we know that a command has been processed and the data has been removed from the rx-buffer.
-                \n\tPros: 
-                     * Safe like Method 1 and optimal like method 2.
-                     * "Not hard" to implement (see their example code in stream.py)   
-                \n\tCons: 
-                     * Unsure how it will interface w/ other methods. Will we need special RX/TX handler functions?
-        """     
+        """Sends a uninterpolated move cmd to the hw_interface for movement of all 6 joints"""
 
         qs = joint_array_sanitizer(q_array)
         # Rapid uninterpolated movement        B and C must be equivalent...
         message = ( f"G0 A{qs[0]:.3f} B{qs[1]:.3f} C{qs[1]:.3f} D{qs[2]:.3f}"
                       f" X{qs[3]:.3f} Y{qs[4]:.3f} Z{qs[5]:.3f}\n")
         
-        with self._rw_lock:
-            self.connection.write(message.encode("UTF-8"))
-            retmsg = str(self.connection.readline().decode("utf-8")).strip("\r\n")
-        if retmsg != "ok":
+        retstr = self.hw_interface.tx_rx(message)
+        if "error" in retstr or "ALARM" in retstr:
             raise RuntimeError("HWManipulator.move_js() returned something other than 'ok'.\n"
-                               f"potential fault msg: {retmsg}")
+                               f"potential fault msg: {retstr}")
 
     def move_ts(self, pose:Pose):
         ikpy_joints = self.IK_Solver(pose)
@@ -151,15 +93,14 @@ class HwManipulator(Manipulator):
         """
         print("Starting position thread")
         while True:
-            if not self.connected: break
-            with self._rw_lock:
-                self.connection.write("?".encode('UTF-8'))
-                dataRead = str(self.connection.readline().decode("utf-8")).strip("\r\n")
-                print(dataRead)
-            if "error" in dataRead or "ALARM" in dataRead:
-                print(f"Error encountered: {dataRead}")
+            if not self.hw_interface.connected or self.stop_thread: break
+            retstr = self.hw_interface.tx_rx("?")
+            print(retstr)
+            if "error" in retstr or "ALARM" in retstr:
+                raise RuntimeError("HWManipulator.update_joint_values() returned something other than 'ok'.\n"
+                               f"potential fault msg: {retstr}")
             else:
-                raw_data = dataRead[1:][:-1].split(",")
+                raw_data = retstr[1:][:-1].split(",")
                 qs = np.array([[float(raw_data[1][5:])], [float(raw_data[2])], [float(raw_data[4])], 
                                [float(raw_data[5])], [float(raw_data[6])], [float(raw_data[7])]])
                 qs *= (np.pi/180.0)
@@ -168,42 +109,9 @@ class HwManipulator(Manipulator):
             time.sleep(0.1) # sleep for 100ms 
     
     def get_joint_values(self) -> np.ndarray:
-        """ Use Gcode M114 to get a string w/ current positions. 
-            May also try the "?" Command as this is what Asgard used
-
-            So Grbl reccomends only querying the position at 5Hz-10Hz maximum.
-            Perhaps a thread is neccesary here to continually get the position and update its associated variable. 
-        """
+        """ Function returns the current position of the end_effector as updated by the position thread """
         retval = None
         with self._pos_lock:
             retval = self.current_position
         return retval
     
-    def get_serial_port(self) -> str:
-        """ Lists serial port names. Method taken from Asgard1.0 serial_port_finder.py 
-            https://github.com/AngelLM/Asgard
-
-            :raises EnvironmentError:
-                On unsupported or unknown platforms
-            :returns:
-                A list of the serial ports available on the system
-        """
-        if sys.platform.startswith('win'):
-            ports = ['COM%s' % (i + 1) for i in range(256)]
-        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-            # this excludes your current terminal "/dev/tty"
-            ports = glob.glob('/dev/tty[A-Za-z]*')
-        elif sys.platform.startswith('darwin'):
-            ports = glob.glob('/dev/tty.*')
-        else:
-            raise EnvironmentError('Unsupported platform')
-
-        result = []
-        for port in ports:
-            try:
-                s = serial.Serial(port)
-                s.close()
-                result.append(port)
-            except (OSError, serial.SerialException):
-                pass
-        return result
